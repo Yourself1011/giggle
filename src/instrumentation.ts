98cbc9fcfe2095a4d2e8279ce4cbab72e552ff33
Daniel Zhang <d.zhang200788@gmail.com>
2024-08-14 11:38:11 -0400

    search screen

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -3,6 +3,6 @@ import index from "./indexer";
 
 export async function register() {
     console.log("SLDkfj");
-    await start(new URL("https://en.wikipedia.org/wiki/Main_Page"));
-    await index();
+    // await start(new URL("https://en.wikipedia.org/wiki/Main_Page"));
+    // await index();
 }
